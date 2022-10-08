6aa30882d26fe20c9813668aa4b5bca4bfc80fc5
MSchvindt <seminare174@gmail.com>
2022-10-08 01:41:38 -0300

    chamber

---
+++ portal_page/chamber/scripts/script.js
@@ -0,0 +1,16 @@
+
+
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate1').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
