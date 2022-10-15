57498590ed7fb8cedca4ba0c07b082b9d18ad974
MSchvindt <seminare174@gmail.com>
2022-10-14 23:38:06 -0300

    menu correction

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -14,3 +14,16 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+//agarro los elementos que necesito para sacar la clase "open" 
+//cuando la resolucion es mayor a 760px
+const hambutton = document.querySelector('#hamburgerBtn');
+const mainnav = document.querySelector('#primaryNav');
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760) {
+        hambutton.classList.remove('open');
+        mainnav.classList.remove('open');
+    }
+};
