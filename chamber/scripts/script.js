187a95e6380b7bb96796047ce149d9e58c67dd3a
MSchvindt <seminare174@gmail.com>
2022-10-17 22:50:20 -0300

    arreglos

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -33,12 +33,12 @@ let weekDay = '';
 weekDay = currentDate.getDay();
 
 function banner(weekDay){
-    if (weekDay === 1 || weekDay === 2){
-        document.getElementsByClassName("banner").style.display = "";
+    if (weekDay == 1 || weekDay == 2){
+        document.getElementsByClassName("banner").style.display = "block";
     } else {
         document.getElementsByClassName("banner").style.display = "none";
-};
-};
+}
+}
 
 document.querySelector(".banner__close").addEventListener("click", function () {
     this.closest(".banner").style.display = "none";
