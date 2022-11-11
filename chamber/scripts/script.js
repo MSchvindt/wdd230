4fc56a6a1ce1004b05334b38d8a0ce01aa6a282b
MSchvindt <104403423+MSchvindt@users.noreply.github.com>
2022-11-11 02:53:22 -0300

    fetch json

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -88,5 +88,82 @@ document.getElementById("hidden").textContent = currentDate;
 //var firstName = Matias;
 //var lvdate = yesterday;
 
-//var string1 = `Welcome back   ${firstName}  ! You last visited on <strong> ${lvdate}  </strong>.`;
-//console.log(string1);
+`Welcome back ${firstName}! You last visited on <strong>${lvdate}</strong>.`;
+
+
+
+//------------directory-----------------------
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
+
+/*const requestPath = '../data.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestPath)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const business = jsonObject['business'];
+        business.forEach(displaybusiness);
+    });
+*/
+
+fetch('https://mschvindt.github.io/wdd230/chamber/data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+
+        json.forEach(element => {
+            displaybusiness(element);
+        });
+    });
+
+function displaybusiness(business) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let name = document.createElement('h2');
+    let address = document.createElement("p");
+    let phone = document.createElement("p")
+    let web = document.createElement("p")
+    let membership = document.createElement("p")
+    let logo = document.createElement('img');
+
+    name.textContent = `${business.name}`;
+    address.textContent = `Address: ${business.addresses}.`;
+    phone.textContent = `Phone Number: ${business.phone_numbers}.`;
+    web.textContent = `Web Page: ${business.website}.`;
+    membership.textContent = `Membership: ${business.embership}.`;
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    logo.setAttribute('src', business.img);
+    logo.setAttribute('alt', `Logo of ${business.name}.`);
+    logo.setAttribute('loading', 'lazy');
+
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(web);
+    card.appendChild(membership);
+    card.appendChild(logo);
+
+    document.querySelector('div.cards').appendChild(card);
+}
